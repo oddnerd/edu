f8949c1a4f6083ca7578b5f0d9131cb60099588e
Tyler St. Onge <tyler@stonge.dev>
2024-01-29 11:13:19 -0500

    test: add templates for trivial merge conditions

--- src/algorithm/merge.rs
+++ src/algorithm/merge.rs
@@ -26,8 +26,9 @@ impl<T: Ord + Clone, Iter: std::iter::Iterator<Item = T>> Iterator for MergeIter
             } else {
                 Some(self.first.next().unwrap().clone())
             }
+        } else {
+            self.second.next()
         }
-        else { self.second.next() }
     }
 }
 
@@ -43,9 +44,47 @@ pub fn merge<T: Ord + Clone>(
         output[offset] = input[low].clone();
     } else {
         let mut auxiliary: Vec<T> = std::vec::Vec::with_capacity(len);
+
         let middle = (low + high) / 2;
+
         let other_middle = middle - low + 1;
+
         merge(input, low, middle, &mut auxiliary, 1);
         merge(input, middle + 1, high, &mut auxiliary, other_middle + 1);
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn mergeiter_left_empty() {}
+
+    #[test]
+    fn mergeiter_right_empty() {}
+
+    #[test]
+    fn mergeiter_left_greater() {}
+
+    #[test]
+    fn mergeiter_right_greater() {}
+
+    #[test]
+    fn mergeiter_back_and_forth() {}
+
+    #[test]
+    fn recursive_left_empty() {}
+
+    #[test]
+    fn recursive_right_empty() {}
+
+    #[test]
+    fn recursive_left_greater() {}
+
+    #[test]
+    fn recursive_right_greater() {}
+
+    #[test]
+    fn recursive_back_and_forth() {}
 }
