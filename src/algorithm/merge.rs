91075042ffe22c3b601c15ae9a82878a460e5b31
Tyler St. Onge <tyler@stonge.dev>
2024-02-01 15:00:55 -0500

    test: add unit tests for inplace merge

--- src/algorithm/merge.rs
+++ src/algorithm/merge.rs
@@ -233,3 +233,43 @@ where
 {
     todo!("that's a rotate");
 }
+
+#[cfg(test)]
+mod inplace_tests {
+    use super::*;
+
+    #[test]
+    fn first_empty() {
+        let mut slice = [0];
+        inplace(&mut slice, 0);
+        assert_eq!(slice, [0]);
+    }
+
+    #[test]
+    fn second_empty() {
+        let mut slice = [0];
+        inplace(&mut slice, 1);
+        assert_eq!(slice, [0]);
+    }
+
+    #[test]
+    fn first_greater() {
+        let mut slice = [1,0];
+        inplace(&mut slice, 1);
+        assert_eq!(slice, [0,1]);
+    }
+
+    #[test]
+    fn second_greater() {
+        let mut slice = [0,1];
+        inplace(&mut slice, 1);
+        assert_eq!(slice, [0,1]);
+    }
+
+    #[test]
+    fn back_and_forth() {
+        let mut slice = [0,3,1,2];
+        inplace(&mut slice, 2);
+        assert_eq!(slice, [0,1,2,3]);
+    }
+}
