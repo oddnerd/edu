375a14042c4fedd39c1703bb0b246058b80722a9
Tyler St. Onge <tyler@stonge.dev>
2024-02-02 16:28:18 -0500

    feat: add sift_down

--- src/algorithm/sort/comparison/heap.rs
+++ src/algorithm/sort/comparison/heap.rs
@@ -22,3 +22,29 @@ fn right_child(index: usize) -> usize {
 fn parent(index: usize) -> usize {
     (index - 1) / 2
 }
+
+/// Repair a binary heap rooted by the node at index `root`.
+fn sift_down<T>(slice: &mut [T], mut root: usize)
+where
+    T: Ord,
+{
+    // while root had at least one child
+    while let Some(left) = slice.get(left_child(root)) {
+        let greatest_child = || -> usize {
+            if slice
+                .get(right_child(root))
+                .is_some_and(|right| left < right)
+            {
+                right_child(root)
+            } else {
+                left_child(root)
+            }
+        }();
+
+        // sift the child down
+        if slice[root] < slice[greatest_child] {
+            slice.swap(root, greatest_child);
+            root = greatest_child;
+        }
+    }
+}
