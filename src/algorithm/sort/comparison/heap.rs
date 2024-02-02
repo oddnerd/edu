83e723b240112776bef8fd9219d1e5a6071c0d24
Tyler St. Onge <tyler@stonge.dev>
2024-02-02 15:45:54 -0500

    feat: add primatives for binary heap

--- src/algorithm/sort/comparison/heap.rs
+++ src/algorithm/sort/comparison/heap.rs
@@ -7,3 +7,18 @@
 //! | worst   | n log n    |
 //! | average | n log n    |
 //! | best    | n log n    |
+
+/// Index of the left child of the node at `index` in a binary heap.
+fn left_child(index: usize) -> usize {
+    2 * index + 1
+}
+
+/// Index of the right child of  the node at`index` in a binary heap.
+fn right_child(index: usize) -> usize {
+    2 * index + 2
+}
+
+/// Index of the parent of the node at `index` in a binary heap.
+fn parent(index: usize) -> usize {
+    (index - 1) / 2
+}
