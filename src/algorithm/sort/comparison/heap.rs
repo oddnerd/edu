2b0a00caba3a34bc55a25e8e011f6c7982c12b71
Tyler St. Onge <tyler@stonge.dev>
2024-02-02 15:21:50 -0500

    chore: add module for heap sort

---
+++ src/algorithm/sort/comparison/heap.rs
@@ -0,0 +1,9 @@
+//! Implementations of [Heap Sort](https://en.wikipedia.org/wiki/Heapsort).
+//!
+//! # Performance
+//!
+//! | Case    | Complexity |
+//! | ------- | ---------- |
+//! | worst   | n log n    |
+//! | average | n log n    |
+//! | best    | n log n    |
