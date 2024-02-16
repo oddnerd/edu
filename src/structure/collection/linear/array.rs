8f4ff7b25cb7072ae0aca79aa1ae1027e981ce5c
Tyler St. Onge <tyler@stonge.dev>
2024-02-16 14:36:47 -0500

    feat: array dereferences to slice

--- src/structure/collection/linear/array.rs
+++ src/structure/collection/linear/array.rs
@@ -3,4 +3,7 @@
 use super::Linear;
 
 /// A [`Linear`] [`Collection`] which occupies contigious memory.
-pub trait Array<'a>: Linear<'a> + std::ops::IndexMut<usize> {}
+pub trait Array<'a>:
+    Linear<'a> + std::ops::IndexMut<usize> + std::ops::DerefMut<Target = [Self::Element]>
+{
+}
