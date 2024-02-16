0540b5d98bae95fd62cb82d8895e26cff699ae78
Tyler St. Onge <tyler@stonge.dev>
2024-02-16 14:38:58 -0500

    feat: array reference convert to slice reference

--- src/structure/collection/linear/array.rs
+++ src/structure/collection/linear/array.rs
@@ -4,6 +4,10 @@ use super::Linear;
 
 /// A [`Linear`] [`Collection`] which occupies contigious memory.
 pub trait Array<'a>:
-    Linear<'a> + std::ops::IndexMut<usize> + std::ops::DerefMut<Target = [Self::Element]>
+    Linear<'a>
+    + std::ops::IndexMut<usize>
+    + std::ops::DerefMut<Target = [Self::Element]>
+    + std::convert::AsMut<[Self::Element]>
+    + std::convert::AsRef<[Self::Element]>
 {
 }
