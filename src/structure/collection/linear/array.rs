1124986fe8abcca9cfba731b6b3f2db28b246be8
Tyler St. Onge <tyler@stonge.dev>
2024-02-16 14:34:06 -0500

    feat: add array trait

--- src/structure/collection/linear/array.rs
+++ src/structure/collection/linear/array.rs
@@ -1,1 +1,6 @@
 //! [Arrays](https://en.wikipedia.org/wiki/Array_(data_type)).
+
+use super::Linear;
+
+/// A [`Linear`] [`Collection`] which occupies contigious memory.
+pub trait Array<'a>: Linear<'a> {}
