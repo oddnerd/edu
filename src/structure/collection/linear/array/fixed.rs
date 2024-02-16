30978eba0374455c912fc1ae11e90cd82a79471e
Tyler St. Onge <tyler@stonge.dev>
2024-02-16 16:36:46 -0500

    feat: turn fixed array into iterator

--- src/structure/collection/linear/array/fixed.rs
+++ src/structure/collection/linear/array/fixed.rs
@@ -14,6 +14,68 @@ impl<'a, T: 'a, const N: usize> super::super::Collection<'a> for Fixed<T, N> {
     }
 }
 
+// By-value [`Iterator`] over a [`Fixed`].
+struct IntoIter<T> {
+    /// pointer to the hypotheical next element.
+    next: *mut T,
+
+    /// pointer to a sentinal end value.
+    end: *mut T,
+}
+
+impl<T> IntoIter<T> {
+    /// Construct an [`IntoIter`] for some [`Fixed`].
+    fn new<const N: usize>(array: Fixed<T, N>) -> Self {
+        Self {
+            next: array.dope,
+            end: array.dope.wrapping_add(N),
+        }
+    }
+}
+
+impl<T> std::ops::Drop for IntoIter<T> {
+    fn drop(&mut self) {
+        while self.next != self.end {
+            // SAFETY:
+            // owns underlying array => valid for reads and writes
+            // `wrapping_add` => pointer is properly aligned
+            // underlying array exists => pointer is non-null
+            // element has no yet been yeilded => valid to drop
+            unsafe {
+                std::ptr::drop_in_place(self.next);
+            }
+        }
+    }
+}
+
+impl<T> std::iter::Iterator for IntoIter<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.next != self.end {
+            // SAFETY:
+            // * input array exists => non-null pointer
+            // * wrapping_add => pointer is aligned
+            // * next != end => pointing to initalized value
+            let next = unsafe { *self.next };
+            self.next = self.next.wrapping_add(1);
+            Some(next)
+        } else {
+            None
+        }
+    }
+}
+
+impl<'a, T: 'a, const N: usize> std::iter::IntoIterator for Fixed<T, N> {
+    type Item = T;
+
+    type IntoIter = IntoIter<T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        Self::IntoIter::new(self)
+    }
+}
+
 /// Immutable reference [`Iterator`] over a [`Fixed`].
 struct Iter<'a, T: 'a> {
     /// pointer to the hypotheical next element.
