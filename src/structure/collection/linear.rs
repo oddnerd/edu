74803c20ce2cbcea0dc2ad7cd798723c259d8610
Tyler St. Onge <tyler@stonge.dev>
2024-02-16 15:53:33 -0500

    fix: iteration takes reference to self

--- src/structure/collection/linear.rs
+++ src/structure/collection/linear.rs
@@ -7,8 +7,8 @@ use super::Collection;
 /// A [`Collection`] whose elements are logically arranged sequentially.
 pub trait Linear<'a>: Collection<'a> + std::iter::IntoIterator {
     /// Iterate over the elements by immutable reference.
-    fn iter() -> impl std::iter::Iterator<Item = &'a Self::Element>;
+    fn iter(&self) -> impl std::iter::Iterator<Item = &'a Self::Element>;
 
     /// Iterate over the elements by mutable reference.
-    fn iter_mut() -> impl std::iter::Iterator<Item = &'a mut Self::Element>;
+    fn iter_mut(&mut self) -> impl std::iter::Iterator<Item = &'a mut Self::Element>;
 }
