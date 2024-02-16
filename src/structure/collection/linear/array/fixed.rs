adab05cfebdd20680f6fb91af38231db79fbce07
Tyler St. Onge <tyler@stonge.dev>
2024-02-16 15:58:43 -0500

    feat: mutable reference iterator

--- src/structure/collection/linear/array/fixed.rs
+++ src/structure/collection/linear/array/fixed.rs
@@ -56,8 +56,54 @@ impl<'a, T> std::iter::Iterator for Iter<'a, T> {
     }
 }
 
+/// Immutable reference [`Iterator`] over a [`Fixed`].
+struct IterMut<'a, T: 'a> {
+    /// pointer to the hypotheical next element.
+    next: *mut T,
+
+    /// pointer to a sentinal end value.
+    end: *mut T,
+
+    /// constrain to lifetime of the underlying [`Fixed`].
+    lifetime: std::marker::PhantomData<&'a mut T>,
+}
+
+impl<'a, T: 'a> IterMut<'a, T> {
+    /// Construct an [`IterMut`] for some [`Fixed`].
+    fn new<const N: usize>(array: &mut Fixed<T, N>) -> Self {
+        Self {
+            next: array.dope,
+            end: array.dope.wrapping_add(N),
+            lifetime: std::marker::PhantomData,
+        }
+    }
+}
+
+impl<'a, T> std::iter::Iterator for IterMut<'a, T> {
+    type Item = &'a mut T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.next != self.end {
+            // SAFETY:
+            // * input array exists => non-null pointer
+            // * wrapping_add => pointer is aligned
+            // * next != end => pointing to initalized value
+            // * lifetime 'a bound to input array => valid lifetime to return
+            let element = unsafe { &mut *self.next };
+            self.next = self.next.wrapping_add(1);
+            Some(element)
+        } else {
+            None
+        }
+    }
+}
+
 impl<'a, T: 'a, const N: usize> super::Linear<'a> for Fixed<T, N> {
     fn iter(&self) -> impl std::iter::Iterator<Item = &'a Self::Element> {
-        Iter::new(&self)
+        Iter::new(self)
+    }
+
+    fn iter_mut(&mut self) -> impl std::iter::Iterator<Item = &'a mut Self::Element> {
+        IterMut::new(self)
     }
 }
