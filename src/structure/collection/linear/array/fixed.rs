0907663485df91a3cb1c4da27e4717f89f4fce0e
Tyler St. Onge <tyler@stonge.dev>
2024-02-16 15:22:54 -0500

    feat: fixed array is a collection

--- src/structure/collection/linear/array/fixed.rs
+++ src/structure/collection/linear/array/fixed.rs
@@ -2,6 +2,14 @@
 
 /// [Dope Vector](https://en.wikipedia.org/wiki/Dope_vector) interpretation of
 /// an array using memory layout to define the structure.
-pub struct Fixed<T> {
+pub struct Fixed<T, const N: usize> {
     dope: *mut T,
+}
+
+impl<'a, T: 'a, const N: usize> super::super::Collection<'a> for Fixed<T, N> {
+    type Element = T;
+
+    fn count() -> usize {
+        N
+    }
 }
