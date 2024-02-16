cb54b750a25d83ff8511f9503e5afb58a16aa14d
Tyler St. Onge <tyler@stonge.dev>
2024-02-16 14:17:54 -0500

    feat: linear collection trait

--- src/structure/collection/linear.rs
+++ src/structure/collection/linear.rs
@@ -1,1 +1,6 @@
 //! Linear collections are ones which logically arrange the elements sequentially.
+
+use super::Collection;
+
+/// A [`Collection`] whose elements are logically arranged sequentially.
+pub trait Linear: Collection {}
