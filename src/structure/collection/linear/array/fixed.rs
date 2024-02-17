2f0607fde7f9e8420e12b52a49a31f6d91ca94ea
Tyler St. Onge <tyler@stonge.dev>
2024-02-16 22:11:21 -0500

    feat: dereference to slice

--- src/structure/collection/linear/array/fixed.rs
+++ src/structure/collection/linear/array/fixed.rs
@@ -193,3 +193,25 @@ impl<T, const N: usize> std::ops::IndexMut<usize> for Fixed<T, N> {
         unsafe { &mut *self.data.as_mut_ptr().add(index) }
     }
 }
+
+impl<T, const N: usize> std::ops::Deref for Fixed<T, N> {
+    type Target = [T];
+
+    fn deref(&self) -> &Self::Target {
+        // SAFETY:
+        // * `data` is aligned => pointer is aligned
+        // * `data` is initalized => every element is initalized
+        // * `data` is one object => slice is over one allocated object
+        unsafe { std::slice::from_raw_parts(self.data.as_ptr(), N) }
+    }
+}
+
+impl<T, const N: usize> std::ops::DerefMut for Fixed<T, N> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        // SAFETY:
+        // * `data` is aligned => pointer is aligned
+        // * `data` is initalized => every element is initalized
+        // * `data` is one object => slice is over one allocated object
+        unsafe { std::slice::from_raw_parts_mut(self.data.as_mut_ptr(), N) }
+    }
+}
