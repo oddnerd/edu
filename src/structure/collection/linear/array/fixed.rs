7bf31370340fc3f2ca4cbf4ba7a4e19a4054a047
Tyler St. Onge <tyler@stonge.dev>
2024-02-17 03:27:40 -0500

    refactor: reuse deref implementation

--- src/structure/collection/linear/array/fixed.rs
+++ src/structure/collection/linear/array/fixed.rs
@@ -226,41 +226,25 @@ impl<T, const N: usize> std::ops::DerefMut for Fixed<T, N> {
 
 impl<T, const N: usize> std::borrow::Borrow<[T]> for Fixed<T, N> {
     fn borrow(&self) -> &[T] {
-        // SAFETY:
-        // * `data` is aligned => pointer is aligned
-        // * `data` is initalized => every element is initalized
-        // * `data` is one object => slice is over one allocated object
-        unsafe { std::slice::from_raw_parts(self.data.as_ptr(), N) }
+        self
     }
 }
 
 impl<T, const N: usize> std::borrow::BorrowMut<[T]> for Fixed<T, N> {
     fn borrow_mut(&mut self) -> &mut [T] {
-        // SAFETY:
-        // * `data` is aligned => pointer is aligned
-        // * `data` is initalized => every element is initalized
-        // * `data` is one object => slice is over one allocated object
-        unsafe { std::slice::from_raw_parts_mut(self.data.as_mut_ptr(), N) }
+        self
     }
 }
 
 impl<T, const N: usize> std::convert::AsRef<[T]> for Fixed<T, N> {
     fn as_ref(&self) -> &[T] {
-        // SAFETY:
-        // * `data` is aligned => pointer is aligned
-        // * `data` is initalized => every element is initalized
-        // * `data` is one object => slice is over one allocated object
-        unsafe { std::slice::from_raw_parts(self.data.as_ptr(), N) }
+        self
     }
 }
 
 impl<T, const N: usize> std::convert::AsMut<[T]> for Fixed<T, N> {
     fn as_mut(&mut self) -> &mut [T] {
-        // SAFETY:
-        // * `data` is aligned => pointer is aligned
-        // * `data` is initalized => every element is initalized
-        // * `data` is one object => slice is over one allocated object
-        unsafe { std::slice::from_raw_parts_mut(self.data.as_mut_ptr(), N) }
+        self
     }
 }
 
