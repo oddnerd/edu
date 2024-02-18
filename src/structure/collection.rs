71aecb337a0575cce433f35a5e1e5163553e9d16
Tyler St. Onge <tyler@stonge.dev>
2024-02-17 20:19:27 -0500

    doc: clarify what a collection is

--- src/structure/collection.rs
+++ src/structure/collection.rs
@@ -1,8 +1,9 @@
-//! [Collections](https://en.wikipedia.org/wiki/Collection_(abstract_data_type)) are data structures which store elements of a shared type together.
+//! [Collections](https://en.wikipedia.org/wiki/Collection_(abstract_data_type))
+//! are data structures which store multiple elements of a sinlge type.
 
 pub mod linear;
 
-/// A collection is a structure to group elements of a single type.
+/// Multiple instances of a single type grouped together.
 pub trait Collection<'a> {
     /// The type of the elements stored within.
     type Element: 'a;
