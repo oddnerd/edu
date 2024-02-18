f0507a07d26342b8bfae6b1560a4bbae5e86ac0f
Tyler St. Onge <tyler@stonge.dev>
2024-02-17 20:35:01 -0500

    doc: use clearer language

--- src/structure/collection/linear.rs
+++ src/structure/collection/linear.rs
@@ -1,10 +1,10 @@
-//! Linear collections are ones which logically arrange the elements sequentially.
+//! Linear [`Collection`] arrange their elements sequentially.
 
 pub mod array;
 
 use super::Collection;
 
-/// A [`Collection`] whose elements are logically arranged sequentially.
+/// A [`Collection`] with sequential ordering.
 pub trait Linear<'a>: Collection<'a> + std::iter::IntoIterator {
     /// Iterate over the elements by immutable reference.
     fn iter(&self) -> impl std::iter::Iterator<Item = &'a Self::Element>;
