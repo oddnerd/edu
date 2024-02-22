3a4944b13a996ad1ef37103566afe11cc27652b1
Tyler St. Onge <tyler@stonge.dev>
2024-02-22 06:28:50 -0500

    feat: collection count

--- src/structure/collection/linear/array/dope.rs
+++ src/structure/collection/linear/array/dope.rs
@@ -1,8 +1,23 @@
 //! Implementation of [`Dope`].
 
+use super::Array;
+use super::Collection;
+use super::Linear;
 
-/// [Dope Vector](https://en.wikipedia.org/wiki/Dope_vector) implementation,
-/// (i.e., [Rust's slice](https://doc.rust-lang.org/std/primitive.slice.html)).
-struct Dope<T, N> {
+/// [Dope Vector](https://en.wikipedia.org/wiki/Dope_vector) implementation
+/// (i.e., [Rust's slice](https://doc.rust-lang.org/std/primitive.slice.html))
+/// uses a pointer-length pair alongside type information to map an array upon
+/// an object buffer in memory.
+pub struct Dope<'a, T: 'a> {
     data: *mut T,
+    len: usize,
+    lifetime: std::marker::PhantomData<&'a T>,
+}
+
+impl<'a, T: 'a> Collection<'a> for Dope<'a, T> {
+    type Element = T;
+
+    fn count(&self) -> usize {
+        self.len
+    }
 }
