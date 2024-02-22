e5723a3b42c5e79fbe7057c9f9b0d1d3450155f8
Tyler St. Onge <tyler@stonge.dev>
2024-02-22 14:33:50 -0500

    feat: iterators for dope vector

--- src/structure/collection/linear/array/dope.rs
+++ src/structure/collection/linear/array/dope.rs
@@ -1,6 +1,5 @@
 //! Implementation of [`Dope`].
 
-use super::Array;
 use super::Collection;
 use super::Linear;
 
@@ -19,5 +18,165 @@ impl<'a, T: 'a> Collection<'a> for Dope<'a, T> {
 
     fn count(&self) -> usize {
         self.len
+    }
+}
+
+/// By-value [`Iterator`] over a [`Dope`].
+pub struct IntoIter<'a, T> {
+    /// ownership of the underlying array.
+    data: Dope<'a, T>,
+
+    /// elements within the range have yet to be yeilded.
+    next: std::ops::Range<std::ptr::NonNull<T>>,
+}
+
+impl<'a, T> IntoIter<'a, T> {
+    /// Construct an [`IntoIter`] for some [`Fixed`].
+    fn new(dope: Dope<'a, T>) -> Self {
+        let mut tmp = Self {
+            data: dope,
+
+            // careful to use pointers to the member and not the parameter.
+            next: std::ptr::NonNull::dangling()..std::ptr::NonNull::dangling(),
+        };
+
+        // SAFETY: the array exists => pointers to it can't be null
+        unsafe {
+            let ptr = tmp.data.data;
+
+            let start = std::ptr::NonNull::new_unchecked(ptr);
+
+            let end = std::ptr::NonNull::new_unchecked(ptr.wrapping_add(tmp.data.len));
+
+            tmp.next = start..end;
+        }
+
+        tmp
+    }
+}
+
+impl<'a, T> std::iter::Iterator for IntoIter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.next.start != self.next.end {
+            // SAFETY: references are bounded by the lifetime of the [`Dope`]
+            let current = unsafe { self.next.start.as_ref() };
+
+            let next = self.next.start.as_ptr().wrapping_add(1);
+
+            // SAFETY: the pointer wasn't null before so it still won't be.
+            let next = unsafe { std::ptr::NonNull::new_unchecked(next) };
+
+            self.next.start = next;
+
+            Some(current)
+        } else {
+            None
+        }
+    }
+}
+
+impl<'a, T: 'a> std::iter::IntoIterator for Dope<'a, T> {
+    type Item = &'a T;
+
+    type IntoIter = IntoIter<'a, T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        Self::IntoIter::new(self)
+    }
+}
+
+/// Immutable reference [`Iterator`] over a [`Dope`].
+pub struct Iter<'a, T: 'a> {
+    /// pointer to the hypotheical next element.
+    next: *const T,
+
+    /// pointer to a sentinal end value.
+    end: *const T,
+
+    /// constrain to lifetime of the underlying [`Dope`].
+    lifetime: std::marker::PhantomData<&'a T>,
+}
+
+impl<'a, T: 'a> Iter<'a, T> {
+    /// Construct an [`Iter`] for some [`Dope`].
+    pub fn new(dope: &Dope<'a, T>) -> Self {
+        Self {
+            next: dope.data,
+            end: dope.data.wrapping_add(dope.len),
+            lifetime: std::marker::PhantomData,
+        }
+    }
+}
+
+impl<'a, T> std::iter::Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.next != self.end {
+            // SAFETY:
+            // * input array exists => non-null pointer
+            // * `wrapping_add` => pointer is aligned
+            // * next != end => pointing to initalized value
+            // * lifetime 'a bound to input array => valid lifetime to return
+            let element = unsafe { &*self.next };
+            self.next = self.next.wrapping_add(1);
+            Some(element)
+        } else {
+            None
+        }
+    }
+}
+
+/// Immutable reference [`Iterator`] over a [`Dope`].
+pub struct IterMut<'a, T: 'a> {
+    /// pointer to the hypotheical next element.
+    next: *mut T,
+
+    /// pointer to a sentinal end value.
+    end: *mut T,
+
+    /// constrain to lifetime of the underlying [`Fixed`].
+    lifetime: std::marker::PhantomData<&'a mut T>,
+}
+
+impl<'a, T: 'a> IterMut<'a, T> {
+    /// Construct an [`IterMut`] for some [`Fixed`].
+    pub fn new(dope: &mut Dope<'a, T>) -> Self {
+        Self {
+            next: dope.data,
+            end: dope.data.wrapping_add(dope.len),
+            lifetime: std::marker::PhantomData,
+        }
+    }
+}
+
+impl<'a, T> std::iter::Iterator for IterMut<'a, T> {
+    type Item = &'a mut T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.next != self.end {
+            // SAFETY:
+            // * input array exists => non-null pointer
+            // * `wrapping_add` => pointer is aligned
+            // * next != end => pointing to initalized value
+            // * lifetime 'a bound to input array => valid lifetime to return
+            let element = unsafe { &mut *self.next };
+            self.next = self.next.wrapping_add(1);
+            Some(element)
+        } else {
+            None
+        }
+    }
+}
+
+impl<'a, T: 'a> Linear<'a> for Dope<'a, T> {
+    fn iter(&self) -> impl std::iter::Iterator<Item = &'a Self::Element> {
+        Iter::new(self)
+    }
+
+    fn iter_mut(&mut self) -> impl std::iter::Iterator<Item = &'a mut Self::Element> {
+        IterMut::new(self)
     }
 }
