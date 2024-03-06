cfddf3fad96dfdcd885420fd21a6584f2ef7c0e8
Tyler St. Onge <tyler@stonge.dev>
2024-03-06 11:56:02 -0500

    test: immutable iterator

--- src/structure/collection/linear/array/dynamic.rs
+++ src/structure/collection/linear/array/dynamic.rs
@@ -813,7 +813,10 @@ mod test {
 
     #[test]
     fn iter() {
-        todo!("construct from something and compare iterators")
+        let array = [0, 1, 2, 3];
+        let instance = Dynamic::from(array.as_slice());
+
+        assert!(instance.iter().eq(array.iter()));
     }
 
     #[test]
