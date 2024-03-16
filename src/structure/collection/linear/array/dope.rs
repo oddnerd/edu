c0b30c4f317b1a7045f8f784e867bda4255813e6
Tyler St. Onge <tyler@stonge.dev>
2024-03-16 03:51:20 -0400

    test: last_mut when empty

--- src/structure/collection/linear/array/dope.rs
+++ src/structure/collection/linear/array/dope.rs
@@ -433,6 +433,18 @@ mod test {
     }
 
     #[test]
+    fn last_mut_yields_none_when_empty() {
+        let underlying: [(); 0] = [];
+        let mut instance = {
+            let ptr = underlying.as_ptr().cast_mut();
+            let ptr = unsafe { std::ptr::NonNull::new_unchecked(ptr) };
+            unsafe { Dope::new(ptr, underlying.len()) }
+        };
+
+        assert_eq!(instance.last_mut(), None);
+    }
+
+    #[test]
     fn index_yields_correct_element() {
         let underlying = [0, 1, 2, 3, 4, 5];
         let instance = {
