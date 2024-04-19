23dd6b9353bade640ecaf2535cf9e366caeef328
Tyler St. Onge <tyler@stonge.dev>
2024-04-19 06:42:33 -0400

    doc: rewrite explanation of List

--- src/structure/collection/linear/list.rs
+++ src/structure/collection/linear/list.rs
@@ -2,7 +2,24 @@
 
 use super::Linear;
 
-/// [`Linear`] [`super::Collection`] which can insert or remove elements.
+/// A modifiable [`Linear`] [`super::Collection`].
+///
+/// Unlike the base [`Linear`] trait, implementors of [`Self`] provide methods
+/// to alter the total [`Collection::count`] of elements.
+///
+/// # Insertion
+///
+/// Elements can be placed before [`Linear::first`] via [`Self::prepend`], and
+/// elements can be placed after [`Linear::last`] via [`Self::append`]. Elements
+/// can be inserted at any given index via [`Self::insert`].
+///
+/// # Removal
+///
+/// The [`List::first`] element can be removed via [`Self::front`] whereas the
+/// [`List::last`] element can be removed via [`Self::back`]. Elements at any
+/// given index can be removed via [`Self::remove`]. Elements can either be
+/// [retained](`Self::retain`) or [withdrawn](`Self::withdraw`) based on a
+/// given predicate. And all elements can be removed via [`Self::clear`].
 pub trait List<'a>:
     Linear<'a>
     + IntoIterator<Item = Self::Element>
