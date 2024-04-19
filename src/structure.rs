5bd51ded1710eeb60445072d8aec9a7f1e4ba99c
Tyler St. Onge <tyler@stonge.dev>
2024-04-19 11:09:45 -0400

    doc: define method

--- src/structure.rs
+++ src/structure.rs
@@ -1,15 +1,16 @@
-//! Data structures.
+//! Logical relationships between data.
 //!
 //! Contained within are [traits][trait] which describe associated behaviour,
 //! and [structs][struct] which relate variables. These types exist to
-//! [encapsulate][encapsulate] (group together) methods (functions) that act
-//! upon [objects][object] (instances of some cohesive _thing_).
+//! [encapsulate][encapsulate] (group together) [methods][method] (functions)
+//! that act upon [objects][object] (instances of some cohesive _thing_).
 //!
 //! See also: [Wikipedia][wikipedia].
 //!
 //! [trait]: https://doc.rust-lang.org/reference/items/traits.html
 //! [struct]: https://doc.rust-lang.org/reference/items/structs.html
 //! [encapsulate]: https://en.wikipedia.org/wiki/Encapsulation_(computer_programming)
+//! [method]: https://en.wikipedia.org/wiki/Method_(computer_programming)
 //! [object]: https://en.wikipedia.org/wiki/Object_(computer_science)
 //! [wikipedia]: https://en.wikipedia.org/wiki/Data_structure
 
