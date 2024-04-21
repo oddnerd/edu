309424981b5b57c3aa4ecbb9fbdfe5b3af29032e
Tyler St. Onge <tyler@stonge.dev>
2024-04-21 06:43:37 -0400

    test: iterator size hint updates

--- src/structure/collection/linear/array/iter/immutable.rs
+++ src/structure/collection/linear/array/iter/immutable.rs
@@ -246,6 +246,24 @@ mod test {
 
                 assert_eq!(actual.len(), expected.len());
             }
+
+            #[test]
+            fn updates() {
+                let mut expected = [0, 1, 2, 3, 4, 5];
+
+                let mut actual = {
+                    let ptr = expected.as_mut_ptr();
+                    let ptr = unsafe { NonNull::new_unchecked(ptr) };
+
+                    unsafe { Iter::new(ptr, expected.len()) }
+                };
+
+                (0..expected.len()).rev().for_each(|len| {
+                    _ = actual.next();
+
+                    assert_eq!(actual.size_hint(), (len, Some(len)));
+                });
+            }
         }
 
         mod fused {
