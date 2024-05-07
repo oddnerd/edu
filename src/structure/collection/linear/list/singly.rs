3e98afe5c22bb1bdf87d836e419cea18eea2e929
Tyler St. Onge <tyler@stonge.dev>
2024-05-07 06:28:36 -0400

    feat: mutable iterator

--- src/structure/collection/linear/list/singly.rs
+++ src/structure/collection/linear/list/singly.rs
@@ -214,3 +214,106 @@ impl<'a, T: 'a> DoubleEndedIterator for Iter<'a, T> {
         Some(&current.element)
     }
 }
+
+/// Mutable [`Iterator`] over the elements within a [`Singly`].
+struct IterMut<'a, T> {
+    /// The next (front) node to yield, if any.
+    next: Option<&'a mut Singly<T>>,
+
+    /// The previously yielded back node, if any.
+    previous_back: Option<&'a Singly<T>>,
+}
+
+impl<'a, T: 'a> Iterator for IterMut<'a, T> {
+    type Item = &'a mut T;
+
+    /// Obtain the next element from the front.
+    ///
+    /// # Performance
+    /// This method take O(1) time and consumes O(1) memory.
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|current| {
+            self.next = current.next.as_mut().map(core::convert::AsMut::as_mut);
+
+            if let (Some(next), Some(back)) = (self.next, self.previous_back) {
+                if core::ptr::addr_eq(next, back) {
+                    self.next = None;
+                }
+            }
+
+            &mut current.element
+        })
+    }
+
+    /// Query how many elements have yet to be yielded.
+    ///
+    /// # Panics
+    /// This method panics if more than [`usize::MAX`] elements.
+    ///
+    /// # Performance
+    /// This method takes O(N) time and consumes O(1) memory.
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (self.len(), Some(self.len()))
+    }
+}
+
+impl<'a, T: 'a> core::iter::FusedIterator for IterMut<'a, T> {}
+
+impl<'a, T: 'a> ExactSizeIterator for IterMut<'a, T> {
+    /// Query how many elements have yet to be yielded.
+    ///
+    /// # Panics
+    /// This method panics if more than [`usize::MAX`] elements.
+    ///
+    /// # Performance
+    /// This method takes O(N) time and consumes O(1) memory.
+    fn len(&self) -> usize {
+        let Some(mut current) = self.next else {
+            return 0;
+        };
+
+        let mut count: usize = 1;
+
+        while let Some(next) = current.next.as_mut() {
+            if let Some(back) = self.previous_back {
+                if core::ptr::addr_eq(next, back) {
+                    break;
+                }
+            }
+
+            current = next;
+
+            if let Some(incremented) = count.checked_add(1) {
+                count = incremented;
+            } else {
+                panic!("too many elements to count");
+            }
+        }
+
+        count
+    }
+}
+
+impl<'a, T: 'a> DoubleEndedIterator for IterMut<'a, T> {
+    /// Obtain the next element from the back.
+    ///
+    /// # Performance
+    /// This method take O(N) time and consumes O(1) memory.
+    fn next_back(&mut self) -> Option<Self::Item> {
+        let mut current = self.next?;
+
+        while let Some(next) = current.next.as_mut() {
+            if let Some(back) = self.previous_back {
+                if core::ptr::addr_eq(next, back) {
+                    break;
+                }
+            }
+
+            current = next;
+        }
+
+        self.previous_back = Some(current);
+
+        Some(&mut current.element)
+    }
+}
