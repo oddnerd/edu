7b09ecf755f9271272fd8157d06c5aff881b797c
Tyler St. Onge <tyler@stonge.dev>
2024-05-07 22:57:59 -0400

    feat: prepend an element

--- src/structure/collection/linear/list/singly.rs
+++ src/structure/collection/linear/list/singly.rs
@@ -4,6 +4,17 @@ pub struct Singly<T> {
     head: Link<T>,
 }
 
+impl<T> Singly<T> {
+    pub fn prepend(&mut self, value: T) {
+        let new = Box::new(Node {
+            element: value,
+            next: core::mem::replace(&mut self.head, Link::Empty),
+        });
+
+        self.head = Link::More(new);
+    }
+}
+
 impl<T> Default for Singly<T> {
     fn default() -> Self {
         Singly { head: Link::Empty }
