5bb2605209c7957e0f79cf40e7b2008cf709b230
Tyler St. Onge <tyler@stonge.dev>
2024-05-22 09:39:43 -0400

    doc: purpose of structs

--- src/structure/collection/linear/list/doubly.rs
+++ src/structure/collection/linear/list/doubly.rs
@@ -2,11 +2,26 @@
 
 use core::ptr::NonNull;
 
+/// Independently allocated elements connected via two links.
+///
+/// Each element exists within separate allocated object, referred to as a
+/// node. Each node contains a pointer to the previous node alongside a pointer
+/// to the subsequent node whereas [`Self`] maintains a pointer to the first
+/// _and_ last node as visualized below:
+///
+/// ```text
+///         +-------+ -> +---------+ -> +---------+ -> ... -> +------+
+/// Self -> | first |    | element |    | element |           | last | <- Self
+///         +-------+ <- +---------+ <- +---------+ <- ... <- +------+
+/// ```
+///
+/// See also: [Wikipedia](https://en.wikipedia.org/wiki/Linked_list)
 pub struct Doubly<T> {
     /// The node considered to be the first, if any are contained.
     elements: Option<NonNull<Node<T>>>,
 }
 
+/// An independently allocated element contained within some [`Doubly`].
 struct Node<T> {
     /// Ownership of the underlying element.
     element: T,
