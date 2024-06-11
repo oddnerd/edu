3c9cc7c92ebd54e74a7a32089421fee7a25ce522
Tyler St. Onge <tyler@stonge.dev>
2024-06-10 22:11:34 -0400

    doc: custom back implementation

--- src/structure/collection/linear/list/singly.rs
+++ src/structure/collection/linear/list/singly.rs
@@ -634,6 +634,26 @@ impl<T> List for Singly<T> {
         Some(removed.element)
     }
 
+    /// Remove the element at the back, the last element, if any.
+    ///
+    /// # Performance
+    /// This method takes O(N) time and consumes O(1) memory.
+    ///
+    /// # Examples
+    /// ```
+    /// use rust::structure::collection::linear::List;
+    /// use rust::structure::collection::linear::list::Singly;
+    ///
+    /// let mut instance = Singly::from_iter([0, 1, 2, 3, 4, 5]);
+    ///
+    /// assert_eq!(instance.back(), Some(5));
+    /// assert_eq!(instance.back(), Some(4));
+    /// assert_eq!(instance.back(), Some(3));
+    /// assert_eq!(instance.back(), Some(2));
+    /// assert_eq!(instance.back(), Some(1));
+    /// assert_eq!(instance.back(), Some(0));
+    /// assert_eq!(instance.back(), None);
+    /// ```
     fn back(&mut self) -> Option<Self::Element> {
         let mut next = &mut self.elements;
 
