a1ed0e50fa46426e14dc7786d82f2eca9631496e
Tyler St. Onge <tyler@stonge.dev>
2024-06-17 08:55:32 -0400

    doc: be concise

--- src/algorithm/sort/comparison/heap.rs
+++ src/algorithm/sort/comparison/heap.rs
@@ -114,18 +114,11 @@ mod max_heapify {
             return;
         }
 
-        // `last` is the parent of the last element hence it is the greatest
-        // index of a node in the heap which has children. Since elements
-        // within `slice[first..]` are leaves to some subtree rooted by an
-        // index in `slice[..=first]`, therefore they can be skipped because
-        // [`sift_down`] orders them when the index of their parent is reached.
-        let last = parent(elements.len() - 1);
+        let last_parent = parent(elements.len() - 1);
 
-        // By going in reverse, since children of `node` will either be leaves
-        // or subtrees already heap ordered, therefore sift it down until the
-        // tree rooted at `node` is itself heap ordered.
-        for node in (0..=last).rev() {
-            sift_down::top_down(&mut elements[node..]);
+        for parent in (0..=last_parent).rev() {
+            // The children of `node` are already heap ordered, so sift down.
+            sift_down::top_down(&mut elements[parent..]);
         }
     }
 
@@ -135,7 +128,7 @@ mod max_heapify {
     /// This method takes O(N) time and consumes O(1) memory.
     pub(super) fn top_down<T: Ord>(elements: &mut [T]) {
         for leaf in 1..=elements.len() {
-            // push leaf into the max-heap
+            // The ancestors of `leaf` are already heap ordered, so sift up.
             sift_up(&mut elements[..leaf]);
         }
     }
