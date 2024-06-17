07dfb12832b5107a571dd5496d5d3a4e3894a294
Tyler St. Onge <tyler@stonge.dev>
2024-06-17 06:37:02 -0400

    doc: remove performance notes detached from code

--- src/algorithm/sort/comparison/heap.rs
+++ src/algorithm/sort/comparison/heap.rs
@@ -1,12 +1,4 @@
 //! Implementations of [Heap Sort](https://en.wikipedia.org/wiki/Heapsort).
-//!
-//! # Performance
-//!
-//! | Case    | Complexity |
-//! | ------- | ---------- |
-//! | worst   | n log n    |
-//! | average | n log n    |
-//! | best    | n log n    |
 
 /// Index of the left child of the node at `index` in a binary heap.
 fn left_child(index: usize) -> usize {
