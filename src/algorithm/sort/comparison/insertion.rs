019972312352ce37b15857096ce4f46818241b50
Tyler St. Onge <tyler@stonge.dev>
2024-06-21 11:33:38 -0400

    chore: method signatures

--- src/algorithm/sort/comparison/insertion.rs
+++ src/algorithm/sort/comparison/insertion.rs
@@ -1,1 +1,9 @@
 //! Implementations of [Insertion Sort](https://en.wikipedia.org/wiki/Insertion_sort).
+
+pub fn iterative<T: Ord>(elements: &mut [T]) {
+    todo!()
+}
+
+pub fn recursive<T: Ord>(elements: &mut [T]) {
+    todo!()
+}
