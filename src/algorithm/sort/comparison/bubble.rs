4315c2f224f86767406ad91079b2e5e69e89269e
Tyler St. Onge <tyler@stonge.dev>
2024-06-24 14:47:52 -0400

    test: bubble sort based on heap sort tests

--- src/algorithm/sort/comparison/bubble.rs
+++ src/algorithm/sort/comparison/bubble.rs
@@ -3,3 +3,73 @@
 pub fn naive<T: Ord>(elements: &mut [T]) {
     todo!()
 }
+
+#[cfg(test)]
+#[allow(
+    clippy::undocumented_unsafe_blocks,
+    clippy::unwrap_used,
+    clippy::expect_used,
+    clippy::assertions_on_result_states,
+    clippy::indexing_slicing
+)]
+mod test {
+    use super::*;
+
+    mod naive {
+        use super::*;
+
+        #[test]
+        fn empty() {
+            let mut elements = [usize::default(); 0];
+
+            naive(&mut elements);
+
+            assert_eq!(elements, []);
+        }
+
+        #[test]
+        fn single_element() {
+            let mut elements = [0];
+
+            naive(&mut elements);
+
+            assert_eq!(elements, [0]);
+        }
+
+        #[test]
+        fn already_sorted() {
+            let mut elements = [0, 1, 2, 3, 4, 5];
+
+            naive(&mut elements);
+
+            assert_eq!(elements, [0, 1, 2, 3, 4, 5]);
+        }
+
+        #[test]
+        fn must_swap() {
+            let mut elements = [1, 0];
+
+            naive(&mut elements);
+
+            assert_eq!(elements, [0, 1]);
+        }
+
+        #[test]
+        fn odd_length() {
+            let mut elements = [2, 1, 0];
+
+            naive(&mut elements);
+
+            assert_eq!(elements, [0, 1, 2]);
+        }
+
+        #[test]
+        fn multiple_swaps() {
+            let mut elements = [2, 0, 3, 1];
+
+            naive(&mut elements);
+
+            assert_eq!(elements, [0, 1, 2, 3]);
+        }
+    }
+}
