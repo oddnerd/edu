bb8766f745aacde68c3ad0a442dd7f10e89e5343
Tyler St. Onge <tyler@stonge.dev>
2024-06-24 15:46:55 -0400

    test: optimized bubble sort

--- src/algorithm/sort/comparison/bubble.rs
+++ src/algorithm/sort/comparison/bubble.rs
@@ -86,4 +86,62 @@ mod test {
             assert_eq!(elements, [0, 1, 2, 3]);
         }
     }
+
+    mod optimized {
+        use super::*;
+
+        #[test]
+        fn empty() {
+            let mut elements = [usize::default(); 0];
+
+            optimized(&mut elements);
+
+            assert_eq!(elements, []);
+        }
+
+        #[test]
+        fn single_element() {
+            let mut elements = [0];
+
+            optimized(&mut elements);
+
+            assert_eq!(elements, [0]);
+        }
+
+        #[test]
+        fn already_sorted() {
+            let mut elements = [0, 1, 2, 3, 4, 5];
+
+            optimized(&mut elements);
+
+            assert_eq!(elements, [0, 1, 2, 3, 4, 5]);
+        }
+
+        #[test]
+        fn must_swap() {
+            let mut elements = [1, 0];
+
+            optimized(&mut elements);
+
+            assert_eq!(elements, [0, 1]);
+        }
+
+        #[test]
+        fn odd_length() {
+            let mut elements = [2, 1, 0];
+
+            optimized(&mut elements);
+
+            assert_eq!(elements, [0, 1, 2]);
+        }
+
+        #[test]
+        fn multiple_swaps() {
+            let mut elements = [2, 0, 3, 1];
+
+            optimized(&mut elements);
+
+            assert_eq!(elements, [0, 1, 2, 3]);
+        }
+    }
 }
