cc134c9beedd068122452dd9560291d518403da8
Tyler St. Onge <tyler@stonge.dev>
2024-06-24 16:35:32 -0400

    doc: naive bubble sort

--- src/algorithm/sort/comparison/bubble.rs
+++ src/algorithm/sort/comparison/bubble.rs
@@ -1,5 +1,24 @@
 //! Implementations of [Bubble Sort](https://en.wikipedia.org/wiki/Bubble_sort).
 
+/// Sort `elements` using naive bubble sort.
+///
+/// Iteratively 'bubble up' the largest yet to be sorted element by iterating
+/// adjacent pairs from the start of the list and swapping the largest element
+/// to the end until in sorted position.
+///
+/// # Performance
+/// This method takes O(N<sup>2</sup>) time and consumes O(1) memory.
+///
+/// # Examples
+/// ```
+/// use rust::algorithm::sort::comparison::bubble::naive;
+///
+/// let mut elements = [0, 5, 2, 3, 1, 4];
+///
+/// naive(&mut elements);
+///
+/// assert_eq!(elements, [0, 1, 2, 3, 4, 5]);
+/// ```
 pub fn naive<T: Ord>(elements: &mut [T]) {
     for sorted in (0..elements.len()).rev() {
         for current_index in 1..=sorted {
@@ -20,17 +39,23 @@ pub fn naive<T: Ord>(elements: &mut [T]) {
     }
 }
 
-#[allow(clippy::indexing_slicing)]
-#[allow(clippy::arithmetic_side_effects)]
 pub fn optimized<T: Ord>(elements: &mut [T]) {
-    let mut swapped;
+    for sorted in 0..elements.len() {
+        let mut swapped = false;
 
-    for i in 0..elements.len() {
-        swapped = false;
+        for current_index in 0..(elements.len() - sorted - 1) {
+            let Some(next_index) = current_index.checked_add(1) else {
+                unreachable!("inner loop prevents ensures within bounds");
+            };
 
-        for j in 0..(elements.len() - i - 1) {
-            if elements[j] > elements[j + 1] {
-                elements.swap(j, j + 1);
+            let (Some(current_element), Some(next_element)) =
+            (elements.get(current_index), elements.get(next_index))
+            else {
+                unreachable!("loops ensure both indexes are within bounds");
+            };
+
+            if current_element > next_element {
+                elements.swap(current_index, next_index);
                 swapped = true;
             }
         }
