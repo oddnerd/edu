fb1083dde91c4b995e943dc6bb014b69e13c6146
Tyler St. Onge <tyler@stonge.dev>
2024-06-26 11:31:06 -0400

    test: cocktail variant based on existing tests

--- src/algorithm/sort/comparison/bubble.rs
+++ src/algorithm/sort/comparison/bubble.rs
@@ -216,4 +216,62 @@ mod test {
             assert_eq!(elements, [0, 1, 2, 3]);
         }
     }
+
+    mod cocktail {
+        use super::*;
+
+        #[test]
+        fn empty() {
+            let mut elements = [usize::default(); 0];
+
+            cocktail(&mut elements);
+
+            assert_eq!(elements, []);
+        }
+
+        #[test]
+        fn single_element() {
+            let mut elements = [0];
+
+            cocktail(&mut elements);
+
+            assert_eq!(elements, [0]);
+        }
+
+        #[test]
+        fn already_sorted() {
+            let mut elements = [0, 1, 2, 3, 4, 5];
+
+            cocktail(&mut elements);
+
+            assert_eq!(elements, [0, 1, 2, 3, 4, 5]);
+        }
+
+        #[test]
+        fn must_swap() {
+            let mut elements = [1, 0];
+
+            cocktail(&mut elements);
+
+            assert_eq!(elements, [0, 1]);
+        }
+
+        #[test]
+        fn odd_length() {
+            let mut elements = [2, 1, 0];
+
+            cocktail(&mut elements);
+
+            assert_eq!(elements, [0, 1, 2]);
+        }
+
+        #[test]
+        fn multiple_swaps() {
+            let mut elements = [2, 0, 3, 1];
+
+            cocktail(&mut elements);
+
+            assert_eq!(elements, [0, 1, 2, 3]);
+        }
+    }
 }
