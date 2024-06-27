9f5778910867a0594bd32e58929f472f6087571c
Tyler St. Onge <tyler@stonge.dev>
2024-06-27 13:03:10 -0400

    test: stable variant of selection sort

--- src/algorithm/sort/comparison/selection.rs
+++ src/algorithm/sort/comparison/selection.rs
@@ -40,6 +40,10 @@ pub fn iterative<T: Ord>(elements: &mut [T]) {
     }
 }
 
+pub fn stable<T: Ord>(elements: &mut [T]) {
+    todo!()
+}
+
 /// Sort `elements` using bidirectional selection sort.
 ///
 /// Note that this is non-stable meaning the order of equivalent elements is
@@ -161,6 +165,64 @@ mod test {
             let mut elements = [2, 0, 3, 1];
 
             iterative(&mut elements);
+
+            assert_eq!(elements, [0, 1, 2, 3]);
+        }
+    }
+
+    mod stable {
+        use super::*;
+
+        #[test]
+        fn empty() {
+            let mut elements: [usize; 0] = [];
+
+            stable(&mut elements);
+
+            assert_eq!(elements, []);
+        }
+
+        #[test]
+        fn single_element() {
+            let mut elements = [0];
+
+            stable(&mut elements);
+
+            assert_eq!(elements, [0]);
+        }
+
+        #[test]
+        fn already_sorted() {
+            let mut elements = [0, 1, 2, 3, 4, 5];
+
+            stable(&mut elements);
+
+            assert_eq!(elements, [0, 1, 2, 3, 4, 5]);
+        }
+
+        #[test]
+        fn must_swap() {
+            let mut elements = [1, 0];
+
+            stable(&mut elements);
+
+            assert_eq!(elements, [0, 1]);
+        }
+
+        #[test]
+        fn odd_length() {
+            let mut elements = [2, 1, 0];
+
+            stable(&mut elements);
+
+            assert_eq!(elements, [0, 1, 2]);
+        }
+
+        #[test]
+        fn multiple_swaps() {
+            let mut elements = [2, 0, 3, 1];
+
+            stable(&mut elements);
 
             assert_eq!(elements, [0, 1, 2, 3]);
         }
