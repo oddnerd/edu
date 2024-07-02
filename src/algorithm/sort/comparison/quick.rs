7527fae00c9f77ef3ccce8b7d0e29bdce968d5de
Tyler St. Onge <tyler@stonge.dev>
2024-07-02 16:27:03 -0400

    doc: lomuto quick sort

--- src/algorithm/sort/comparison/quick.rs
+++ src/algorithm/sort/comparison/quick.rs
@@ -1,22 +1,49 @@
 //! Implementations of [Quick Sort](https://en.wikipedia.org/wiki/Quicksort).
 
-/// TODO
-pub fn lomuto<T: Ord + Clone>(elements: &mut [T]) {
-    /// TODO
-    fn partition<T: Ord + Clone>(elements: &mut [T]) -> usize {
-        let Some((pivot, remaining)) = elements.split_last_mut() else {
+/// Sort `elements` using quick sort with Lomuto's partition scheme.
+///
+/// Note that this is non-stable meaning the order of equivalent elements is
+/// not preserved.
+///
+/// Place the last element into sorted order by partitioning, i.e., placing
+/// smaller elements before it and greater elements after it. Recursively sort
+/// the remaining unsorted sub-lists placing one element into sorted order for
+/// each call until all elements are sorted.
+///
+/// # Performance
+/// This method takes O(N<sup>2</sup>) time and consumes O(N) memory.
+///
+/// # Examples
+/// ```
+/// use rust::algorithm::sort::comparison::quick::lomuto;
+///
+/// let mut elements = [0, 5, 2, 3, 1, 4];
+///
+/// lomuto(&mut elements);
+///
+/// assert_eq!(elements, [0, 1, 2, 3, 4, 5]);
+/// ```
+pub fn lomuto<T: Ord>(elements: &mut [T]) {
+    /// Partition `elements` based on the last element.
+    ///
+    /// This places the last element into sorted position, as well as ordering
+    /// the remaining elements such that those less than it come before and
+    /// those greater come after.
+    fn partition<T: Ord>(elements: &mut [T]) -> usize {
+        let Some((pivot_element, elements)) = elements.split_last_mut() else {
             unreachable!("caller is responsible for ensuring not empty");
         };
 
+        // The sorted position for the pivot element.
         let mut pivot_index = 0;
 
-        for current_index in 0..remaining.len() {
-            let Some(current_elements) = remaining.get(current_index) else {
+        for index in 0..elements.len() {
+            let Some(element) = elements.get(index) else {
                 unreachable!("loop ensures index is within bounds");
             };
 
-            if current_elements <= pivot {
-                remaining.swap(current_index, pivot_index);
+            if element <= pivot_element {
+                elements.swap(index, pivot_index);
 
                 if let Some(incremented) = pivot_index.checked_add(1) {
                     pivot_index = incremented;
@@ -26,7 +53,15 @@ pub fn lomuto<T: Ord + Clone>(elements: &mut [T]) {
             }
         }
 
-        elements.swap(pivot_index, elements.len() - 1);
+        if let Some(temporary_pivot) = elements.get_mut(pivot_index) {
+            core::mem::swap(temporary_pivot, pivot_element);
+        } else {
+            debug_assert_eq!(
+                pivot_index,
+                elements.len(),
+                "pivot is already sorted as the last overall element"
+            );
+        }
 
         pivot_index
     }
@@ -35,12 +70,13 @@ pub fn lomuto<T: Ord + Clone>(elements: &mut [T]) {
         return;
     }
 
+    // Place the pivot element into sorted position.
     let pivot = partition(elements);
 
     let (left, right) = elements.split_at_mut(pivot);
 
-    let Some((_, right)) = right.split_first_mut() else {
-        todo!();
+    let Some((_pivot, right)) = right.split_first_mut() else {
+        unreachable!("contains at least the pivot element");
     };
 
     lomuto(left);
