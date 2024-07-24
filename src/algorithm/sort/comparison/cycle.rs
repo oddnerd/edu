952703c58d22ac574a6b67e8d17d8d74b4e378f4
Tyler St. Onge <tyler@stonge.dev>
2024-07-24 18:02:28 -0400

    test: cycle sort

--- src/algorithm/sort/comparison/cycle.rs
+++ src/algorithm/sort/comparison/cycle.rs
@@ -1,43 +1,71 @@
 //! Implementations of [Cycle Sort](https://en.wikipedia.org/wiki/Cycle_sort).
 
-pub fn cycle<T: Ord + Clone>(elements: &mut [T]) {
-    for start in 0..elements.len() {
-        let mut item = elements[start].clone();
+pub(super) fn cycle<T: Ord + Clone>(elements: &mut [T]) {
+    todo!()
+}
 
-        let mut pos = start;
+#[cfg(test)]
+#[allow(
+    clippy::undocumented_unsafe_blocks,
+    clippy::unwrap_used,
+    clippy::expect_used,
+    clippy::assertions_on_result_states,
+    clippy::indexing_slicing
+)]
+mod test {
+    use super::*;
 
-        for element in &elements[start + 1..] {
-            if element < &item {
-                pos += 1;
-            }
-        }
+    #[test]
+    fn empty() {
+        let mut elements: [usize; 0] = [];
 
-        if pos == start {
-            continue;
-        }
+        cycle(&mut elements);
 
-        while item == elements[pos] {
-            pos += 1;
-        }
-
-        core::mem::swap(&mut elements[pos], &mut item);
-
-        while pos != start {
-            pos = start;
-
-            for i in (start+1)..elements.len() {
-                if elements[i] < item {
-                    pos += 1;
-                }
-            }
-
-            while item == elements[pos] {
-                pos += 1;
-            }
-
-            core::mem::swap(&mut elements[pos], &mut item);
-        }
-
+        assert_eq!(elements, []);
     }
 
+    #[test]
+    fn single_element() {
+        let mut elements = [0];
+
+        cycle(&mut elements);
+
+        assert_eq!(elements, [0]);
+    }
+
+    #[test]
+    fn already_sorted() {
+        let mut elements = [0, 1, 2, 3, 4, 5];
+
+        cycle(&mut elements);
+
+        assert_eq!(elements, [0, 1, 2, 3, 4, 5]);
+    }
+
+    #[test]
+    fn must_swap() {
+        let mut elements = [1, 0];
+
+        cycle(&mut elements);
+
+        assert_eq!(elements, [0, 1]);
+    }
+
+    #[test]
+    fn odd_length() {
+        let mut elements = [2, 1, 0];
+
+        cycle(&mut elements);
+
+        assert_eq!(elements, [0, 1, 2]);
+    }
+
+    #[test]
+    fn multiple_swaps() {
+        let mut elements = [2, 0, 3, 1];
+
+        cycle(&mut elements);
+
+        assert_eq!(elements, [0, 1, 2, 3]);
+    }
 }
