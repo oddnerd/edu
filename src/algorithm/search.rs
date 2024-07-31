4b7712725ff1af938447737d9e66188b31399af4
Tyler St. Onge <tyler@stonge.dev>
2024-07-31 06:40:00 -0400

    test: search algorithms

--- src/algorithm/search.rs
+++ src/algorithm/search.rs
@@ -1,1 +1,71 @@
 //! Implementations of [search](https://en.wikipedia.org/wiki/Search_algorithm).
+
+fn linear<T: Ord>(haystack: &[T], needle: &T) -> Option<usize> {
+    todo!()
+}
+
+fn binary<T: Ord>(haystack: &[T], needle: &T) -> Option<usize> {
+    todo!()
+}
+
+#[cfg(test)]
+#[allow(
+    clippy::undocumented_unsafe_blocks,
+    clippy::unwrap_used,
+    clippy::expect_used,
+    clippy::assertions_on_result_states,
+    clippy::indexing_slicing
+)]
+mod test {
+    use super::*;
+
+    mod linear {
+        use super::*;
+
+        #[test]
+        fn empty() {
+            let elements: [usize; 0] = [];
+
+            assert_eq!(linear(&elements, &0), None);
+        }
+
+        #[test]
+        fn yields_correct_index_when_contained() {
+            let elements = [0, 1, 2, 3, 4, 5];
+
+            assert_eq!(linear(&elements, &2), Some(2));
+        }
+
+        #[test]
+        fn yields_none_when_not_contained() {
+            let elements = [0, 1, 2, 3, 4, 5];
+
+            assert_eq!(linear(&elements, &6), None);
+        }
+    }
+
+    mod binary {
+        use super::*;
+
+        #[test]
+        fn empty() {
+            let elements: [usize; 0] = [];
+
+            assert_eq!(binary(&elements, &0), None);
+        }
+
+        #[test]
+        fn yields_correct_index_when_contained() {
+            let elements = [0, 1, 2, 3, 4, 5];
+
+            assert_eq!(binary(&elements, &2), Some(2));
+        }
+
+        #[test]
+        fn yields_none_when_not_contained() {
+            let elements = [0, 1, 2, 3, 4, 5];
+
+            assert_eq!(binary(&elements, &6), None);
+        }
+    }
+}
