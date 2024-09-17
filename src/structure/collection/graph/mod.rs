66a356f25ec2236a8861184ec64e8328a10980f6
Tyler St. Onge <tyler@stonge.dev>
2024-09-17 18:58:36 -0400

    feat: obtain nodes from edges and vice versa

--- src/structure/collection/graph/mod.rs
+++ src/structure/collection/graph/mod.rs
@@ -4,10 +4,14 @@ use super::Collection;
 
 pub trait Node {
     type Edge: Edge;
+
+    fn edges(&self) -> impl Iterator<Item = &impl Edge>;
 }
 
 pub trait Edge {
     type Node: Node;
+
+    fn nodes(&self) -> (&impl Node, &impl Node);
 }
 
 pub trait Graph : Collection {
