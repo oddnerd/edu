107eff61a04d8990ad5f2fc2235763501400df3d
Tyler St. Onge <tyler@stonge.dev>
2024-09-17 19:04:38 -0400

    feat: mutable variants

--- src/structure/collection/graph/mod.rs
+++ src/structure/collection/graph/mod.rs
@@ -6,12 +6,16 @@ pub trait Node {
     type Edge: Edge;
 
     fn edges(&self) -> impl Iterator<Item = &impl Edge>;
+
+    fn edges_mut(&mut self) -> impl Iterator<Item = &mut impl Edge>;
 }
 
 pub trait Edge {
     type Node: Node;
 
     fn nodes(&self) -> (&impl Node, &impl Node);
+
+    fn nodes_mut(&mut self) -> (&mut impl Node, &mut impl Node);
 }
 
 pub trait Graph : Collection {
