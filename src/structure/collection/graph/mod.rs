da6b868d757806c45f293447f45b4e4fda6f0c35
Tyler St. Onge <tyler@stonge.dev>
2024-09-17 18:34:44 -0400

    feat: associated node and edge types

--- src/structure/collection/graph/mod.rs
+++ src/structure/collection/graph/mod.rs
@@ -2,4 +2,16 @@
 
 use super::Collection;
 
-pub trait Graph : Collection {}
+pub trait Node {
+    type Edge: Edge;
+}
+
+pub trait Edge {
+    type Node: Node;
+}
+
+pub trait Graph : Collection {
+    type Node: Node<Edge = Self::Edge>;
+
+    type Edge: Edge<Node = Self::Node>;
+}
