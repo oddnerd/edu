b219c9066a0e4fb0010f245cce9d21b312135754
Tyler St. Onge <tyler@stonge.dev>
2024-09-17 19:58:46 -0400

    doc: getter methods

--- src/structure/collection/graph/mod.rs
+++ src/structure/collection/graph/mod.rs
@@ -7,8 +7,10 @@ pub trait Node {
     /// The type connecting this [`Node`] to others in a [`Graph`].
     type Edge: Edge;
 
+    /// Immutably query the [`Edge`] associating this [`Node`] with another.
     fn edges(&self) -> impl Iterator<Item = &impl Edge>;
 
+    /// Mutably query the [`Edge`] associating this [`Node`] with another.
     fn edges_mut(&mut self) -> impl Iterator<Item = &mut impl Edge>;
 }
 
@@ -17,8 +19,10 @@ pub trait Edge {
     /// The type this [`Edge`] connects together.
     type Node: Node;
 
+    /// Immutably obtain the [`Node`] connected via this [`Edge`].
     fn nodes(&self) -> (&impl Node, &impl Node);
 
+    /// Mutably obtain the [`Node`] connected via this [`Edge`].
     fn nodes_mut(&mut self) -> (&mut impl Node, &mut impl Node);
 }
 
@@ -30,9 +34,15 @@ pub trait Graph : Collection {
     /// The type associating elements.
     type Edge: Edge<Node = Self::Node>;
 
+    /// Immutable obtain all [`Node`] contained within this [`Graph`].
     fn nodes(&self) -> impl Iterator<Item = &impl Node>;
+
+    /// Mutably obtain all [`Node`] contained within this [`Graph`].
     fn nodes_mut(&mut self) -> impl Iterator<Item = &mut impl Node>;
 
+    /// Immutable obtain all [`Edge`] contained within this [`Graph`].
     fn edges(&self) -> impl Iterator<Item = &impl Edge>;
+
+    /// Mutably obtain all [`Edge`] contained within this [`Graph`].
     fn edges_mut(&mut self) -> impl Iterator<Item = &mut impl Edge>;
 }
