5220f484e6fcee0a8a4eb19cc4cf8b2e508becda
Tyler St. Onge <tyler@stonge.dev>
2024-09-18 19:48:24 -0400

    feat: methods for nodes in a tree

--- src/structure/collection/graph/tree/mod.rs
+++ src/structure/collection/graph/tree/mod.rs
@@ -6,12 +6,33 @@ use super::Graph;
 ///
 /// TODO: explain recursive nature.
 pub trait Tree : Graph<Node = Self> {
-    /// Immutably obtain the top-level [`Node`].
+    /// Immutably obtain the [`Node`] whose parent is `self`.
+    fn children<'a>(&'a self) -> impl Iterator<Item = &'a Self::Node> where Self::Node: 'a;
+
+    /// Mutably obtain the [`Node`] whose parent is `self`.
+    fn children_mut<'a>(&'a mut self) -> impl Iterator<Item = &'a mut Self::Node> where Self::Node: 'a;
+
+    /// Immutably obtain the [`Node`] for which `self` is a child.
+    fn parent(&self) -> &Self::Node;
+
+    /// Mutably obtain the [`Node`] for which `self` is a child.
+    fn parent_mut(&mut self) -> &mut Self::Node;
+
+    /// Immutably obtain the top-level [`Node`] of the entire [`Tree`].
     fn root(&self) -> &Self::Node;
 
-    /// Mutably obtain the top-level [`Node`].
+    /// Mutably obtain the top-level [`Node`] of the entire [`Tree`].
     fn root_mut(&mut self) -> &mut Self::Node;
 
-    /// Query the length of the longest chain of [`Node`].
+    /// Query the longest length of chain of [`Node`] from `self` to a leaf.
     fn height(&self) -> usize;
+
+    /// Query the length of the chain of [`Node`] between the root and `self`.
+    fn level(&self) -> usize;
+
+    /// Query the maximum number of children a [`Node`] can have.
+    fn degree(&self) -> usize;
+
+    /// Query the number of siblings this [`Node`] has.
+    fn width(&self) -> usize;
 }
