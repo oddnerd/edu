ba6041b75a15f7281612231fef490129a9530b73
Tyler St. Onge <tyler@stonge.dev>
2024-09-20 10:32:40 -0400

    feat: basic graph trait

--- src/structure/collection/graph/mod.rs
+++ src/structure/collection/graph/mod.rs
@@ -1,13 +1,30 @@
 //! Implementations of [`Graph`].
 
+// TODO: This temporarily disables aggressive warnings within the module for
+// the sake of prototyping. This should be removed thereafter, thenceforth
+// dealing with each warning individually before even considering merging.
+#![allow(missing_docs)]
+
 pub mod tree;
 
 pub use tree::Tree;
 
 use super::Collection;
 
-pub trait Graph : Collection {}
+pub trait Graph : Collection {
+    type Node: for<'a> Node<Edge<'a> = Self::Edge<'a>>;
 
-pub trait Node {}
+    type Edge<'a>: Edge<'a, Node = Self::Node>;
+}
 
-pub trait Edge<'a> {}
+pub trait Node {
+    type Edge<'a>: Edge<'a> where Self: 'a;
+
+    fn edges(&self) -> impl Iterator<Item = Self::Edge<'_>>;
+}
+
+pub trait Edge<'a> {
+    type Node: Node;
+
+    fn nodes(&self) -> (&Self::Node, &Self::Node);
+}
