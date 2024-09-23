09bdda5a84bd78decf43d057f6297de0eee50791
Tyler St. Onge <tyler@stonge.dev>
2024-09-23 18:27:29 -0400

    doc: directed graph node

--- src/structure/collection/graph/directed.rs
+++ src/structure/collection/graph/directed.rs
@@ -2,18 +2,24 @@
 
 use super::Graph;
 
+/// A [`Graph`] whose [`Edge`] has a direction from one [`Node`] to the other.
 pub trait Directed : for<'a> Graph<Edge<'a>: Edge<'a>, Node: Node> {}
 
+/// An instance of an element within a [`Directed`] [`Graph`].
 pub trait Node : super::Node {
+    /// Query if `self` links towards another [`Node`].
     #[must_use]
     fn is_internal(&self) -> bool;
 
+    /// Query if `self` does _not_ link towards another [`Node`].
     #[must_use]
     fn is_external(&self) -> bool;
 
+    /// Query how many [`Node`] connect towards `self`.
     #[must_use]
     fn in_degree(&self) -> usize;
 
+    /// Query how many [`Node`] `self` connects towards.
     #[must_use]
     fn out_degree(&self) -> usize;
 }
