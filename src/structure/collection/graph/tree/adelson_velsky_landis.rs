942c33f465b1a439a5fdd5584ca8bb70029746cc
Tyler St. Onge <tyler@stonge.dev>
2024-09-23 14:48:02 -0400

    test: inserting when empty creates root

--- src/structure/collection/graph/tree/adelson_velsky_landis.rs
+++ src/structure/collection/graph/tree/adelson_velsky_landis.rs
@@ -17,3 +17,32 @@ struct Node<T> {
     left: Option<Box<Node<T>>>,
     right: Option<Box<Node<T>>>,
 }
+
+#[cfg(test)]
+#[allow(
+    clippy::undocumented_unsafe_blocks,
+    clippy::unwrap_used,
+    clippy::expect_used
+)]
+mod test {
+    use super::*;
+
+    mod method {
+        use super::*;
+
+        mod insert {
+            use super::*;
+
+            #[test]
+            fn inserts_root_node_when_empty() {
+                let mut instance = AdelsonVelsoLandis::<i32> { root: None };
+
+                instance.insert(12345);
+
+                assert!(instance.root.is_some());
+                assert_eq!(instance.root.unwrap().data, 12345);
+            }
+
+        }
+    }
+}
