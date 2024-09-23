cf10fc8f32e5b0794043a7d60babbe7edb548146
Tyler St. Onge <tyler@stonge.dev>
2024-09-23 15:23:15 -0400

    doc: graph

--- src/structure/collection/graph/mod.rs
+++ src/structure/collection/graph/mod.rs
@@ -8,13 +8,18 @@ pub use directed::Directed;
 
 use super::Collection;
 
+/// A [`Collection`] of [`Node`] that are associated with via [`Edge`].
 pub trait Graph : Collection {
+    /// The type representing an [`Self::Element`].
     type Node: for<'a> Node<Edge<'a> = Self::Edge<'a>>;
 
+    /// The type which associates [`Node`] with one another.
     type Edge<'a>: Edge<'a, Node = Self::Node>;
 
+    /// Obtain all [`Edge`] contained within `self`.
     fn edges(&self) -> impl Iterator<Item = Self::Edge<'_>>;
 
+    /// Obtain all [`Node`] contained within `self`.
     fn nodes(&self) -> impl Iterator<Item = Self::Node>;
 }
 
