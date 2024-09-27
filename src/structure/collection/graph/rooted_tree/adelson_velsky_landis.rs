77b123d34a562c2cd623e8b3c08393238ec40cd0
Tyler St. Onge <tyler@stonge.dev>
2024-09-26 22:35:05 -0400

    revert: that was a fragile implementation

--- src/structure/collection/graph/rooted_tree/adelson_velsky_landis.rs
+++ src/structure/collection/graph/rooted_tree/adelson_velsky_landis.rs
@@ -207,72 +207,17 @@ struct Node<T> {
 }
 
 impl<T> Node<T> {
-    /// Balance a [`Node`] whose right branch has greater height than the left.
-    ///
-    /// # Errors
-    /// Makes no changes if `self`
-    ///
-    /// * has a left child
-    /// * does not have a right child
-    /// * the right child has a left child
-    /// * the right child does not itself have a right child
+    /// TODO
     ///
     /// # Performance
     /// This method takes O(1) time and consumes O(1) memory.
     ///
     /// # Examples
     /// ```
-    /// let mut root = Node {
-    ///     element: 0,
-    ///     highest_branch: BalanceFactor::Right,
-    ///     parent: None,
-    ///     left: None,
-    ///     right: None,
-    /// };
-    ///
-    /// let right = root.right.insert(Box::new(Node {
-    ///     element: 1,
-    ///     highest_branch: BalanceFactor::Right,
-    ///     parent: Some(core::ptr::NonNull::from(&root)),
-    ///     left: None,
-    ///     right: None,
-    /// }));
-    ///
-    ///
-    /// right.right = Some(Box::new(Node {
-    ///     element: 2,
-    ///     highest_branch: BalanceFactor::Balanced,
-    ///     parent: Some(core::ptr::NonNull::from(right.as_ref())),
-    ///     left: None,
-    ///     right: None
-    /// }));
-    ///
-    /// assert!(root.rotate_left().is_ok());
-    ///
-    /// assert_eq!(root.element, 1);
-    /// assert!(root.left.as_deref().is_some_and(|left| left.element == 0));
-    /// assert!(root.right.as_deref().is_some_and(|right| right.element == 2));
+    /// todo!()
     /// ```
-    fn rotate_left(&mut self) -> Result<(), ()> {
-        if self.left.is_some() {
-            return Err(());
-        }
-
-        let Some(child) = self.right.as_deref_mut() else {
-            return Err(());
-        };
-
-        let Some(grandchild) = child.right.as_deref_mut() else {
-            return Err(());
-        };
-
-        grandchild.parent = child.parent;
-
-        self.left = child.right.take();
-
-        core::mem::swap(&mut self.element, &mut child.element);
-
-        Ok(())
+    fn rotate_left(root: Box<Self>) -> Box<Self> {
+        todo!()
     }
 }
 
