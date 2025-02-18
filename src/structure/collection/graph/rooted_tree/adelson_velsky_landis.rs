f0ac79cf268cbb623ad58f0c0dea234e83539792
Tyler St. Onge <tyler@stonge.dev>
2025-02-18 16:08:13 -0500

    test: extending to the left via left leaf

--- src/structure/collection/graph/rooted_tree/adelson_velsky_landis.rs
+++ src/structure/collection/graph/rooted_tree/adelson_velsky_landis.rs
@@ -583,27 +583,90 @@ mod test {
                             /// invoke no rotation therefore remaining the
                             /// same structure.
                             fn setup() -> AdelsonVelsoLandis<usize> {
-                                todo!()
+                                let mut instance = AdelsonVelsoLandis::default();
+
+                                assert!(instance.insert(3).is_ok());
+
+                                assert!(instance.insert(2).is_ok());
+                                assert!(instance.insert(4).is_ok());
+
+                                assert!(instance.insert(1).is_ok());
+
+                                instance
                             }
 
                             #[test]
                             fn root() {
-                                todo!()
+                                let instance = setup();
+
+                                // SAFETY: no other reference exists to this node to alias.
+                                let root = unsafe { instance.root.unwrap().as_ref() };
+
+                                assert_eq!(root.element, 3);
+                                assert_eq!(root.balance, BalanceFactor::Left);
+                                assert_eq!(root.parent, None);
+                                assert!(root.left.is_some());
+                                assert!(root.right.is_some());
                             }
 
                             #[test]
                             fn left_child() {
-                                todo!()
+                                let instance = setup();
+
+                                let root_ptr = instance.root.unwrap();
+
+                                // SAFETY: no other reference exists to this node to alias.
+                                let root = unsafe { root_ptr.as_ref() };
+
+                                // SAFETY: no other reference exists to this node to alias.
+                                let left = unsafe { root.left.unwrap().as_ref() };
+
+                                assert_eq!(left.element, 2);
+                                assert_eq!(left.balance, BalanceFactor::Left);
+                                assert_eq!(left.parent, Some(root_ptr));
+                                assert!(left.left.is_some());
+                                assert!(left.right.is_none());
                             }
 
                             #[test]
                             fn left_left_grandchild() {
-                                todo!()
+                                let instance = setup();
+
+                                // SAFETY: no other reference exists to this node to alias.
+                                let root = unsafe { instance.root.unwrap().as_ref() };
+
+                                let left_ptr = root.left.unwrap();
+
+                                // SAFETY: no other reference exists to this node to alias.
+                                let left = unsafe { left_ptr.as_ref() };
+
+                                // SAFETY: no other reference exists to this node to alias.
+                                let left_left = unsafe { left.left.unwrap().as_ref() };
+
+                                assert_eq!(left_left.element, 1);
+                                assert_eq!(left_left.balance, BalanceFactor::Balanced);
+                                assert_eq!(left_left.parent, Some(left_ptr));
+                                assert!(left_left.left.is_none());
+                                assert!(left_left.right.is_none());
                             }
 
                             #[test]
                             fn right_child() {
-                                todo!()
+                                let instance = setup();
+
+                                let root_ptr = instance.root.unwrap();
+
+                                // SAFETY: no other reference exists to this node to alias.
+                                let root = unsafe { root_ptr.as_ref() };
+
+                                // SAFETY: no other reference exists to this node to alias.
+                                let right = unsafe { root.left.unwrap().as_ref() };
+
+                                assert_eq!(right.element, 4);
+                                assert_eq!(right.balance, BalanceFactor::Balanced);
+                                assert_eq!(right.parent, Some(root_ptr));
+                                assert!(right.left.is_none());
+                                assert!(right.right.is_none());
                             }
                         }
 
