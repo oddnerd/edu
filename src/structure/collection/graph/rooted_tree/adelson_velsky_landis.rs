fd1038d2e6407eec31eaf9c28378aebf8c94491d
Tyler St. Onge <tyler@stonge.dev>
2025-02-19 11:14:31 -0500

    test: left child after right rotation

--- src/structure/collection/graph/rooted_tree/adelson_velsky_landis.rs
+++ src/structure/collection/graph/rooted_tree/adelson_velsky_landis.rs
@@ -2115,7 +2115,21 @@ mod test {
 
                     #[test]
                     fn left_child() {
-                        todo!()
+                        let instance = setup();
+
+                        let root_ptr = instance.root.unwrap();
+
+                        // SAFETY: no other reference exists to this node to alias.
+                        let root = unsafe { root_ptr.as_ref() };
+
+                        // SAFETY: no other reference exists to this node to alias.
+                        let left = unsafe { root.left.unwrap().as_ref() };
+
+                        assert_eq!(left.element, 2);
+                        assert_eq!(left.balance, BalanceFactor::Left);
+                        assert_eq!(left.parent, Some(root_ptr));
+                        assert!(left.left.is_some());
+                        assert!(left.right.is_none());
                     }
 
                     #[test]
