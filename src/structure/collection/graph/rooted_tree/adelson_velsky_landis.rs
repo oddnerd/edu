8bf4ff090e367f30b98b461484ab8bc610916f63
Tyler St. Onge <tyler@stonge.dev>
2025-02-21 17:40:09 -0500

    test: setup for removal causing left rotation

--- src/structure/collection/graph/rooted_tree/adelson_velsky_landis.rs
+++ src/structure/collection/graph/rooted_tree/adelson_velsky_landis.rs
@@ -3655,7 +3655,24 @@ mod test {
                     /// 2  4 5  8
                     /// ```
                     fn setup() -> AdelsonVelsoLandis<usize> {
-                        todo!()
+                        let mut instance = AdelsonVelsoLandis::default();
+
+                        assert!(instance.insert(3).is_ok());
+
+                        assert!(instance.insert(2).is_ok());
+                        assert!(instance.insert(6).is_ok());
+
+                        assert!(instance.insert(1).is_ok());
+
+                        assert!(instance.insert(4).is_ok());
+                        assert!(instance.insert(7).is_ok());
+
+                        assert!(instance.insert(5).is_ok());
+                        assert!(instance.insert(8).is_ok());
+
+                        assert_eq!(instance.remove(&1), Some(1));
+
+                        instance
                     }
 
                     #[test]
