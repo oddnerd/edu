387d608d1f095049ac45a9f1e6adae785a6cd291
Tyler St. Onge <tyler@stonge.dev>
2025-03-12 11:41:30 -0400

    chore: globally disable some lints during tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,16 @@
 //! Hand-written implementations in Rust for personal reference.
 
+// Disable particularly aggressive lints exclusively for tests.
+#![cfg_attr(
+    test,
+    allow(
+        clippy::undocumented_unsafe_blocks,
+        clippy::unwrap_used,
+        clippy::expect_used,
+        clippy::assertions_on_result_states,
+        clippy::indexing_slicing
+    )
+)]
+
 pub mod algorithm;
 pub mod structure;
