61551c9c4626d3b3ae9ac507e811d97c37994cdd
Tyler St. Onge <tyler@stonge.dev>
2025-03-20 03:07:53 -0400

    doc: worst, best, and average performance

--- src/algorithm/sort/bubble.rs
+++ src/algorithm/sort/bubble.rs
@@ -175,7 +175,15 @@ pub fn bidirectional<T: Ord>(elements: &mut [T]) {
 /// non-overlapping chunks of two elements
 ///
 /// # Performance
-/// This method takes O(N<sup>2</sup>) time and consumes O(1) memory.
+/// #### Time Complexity
+/// | Worst | Best | Average |
+/// | :-: | :-: | :-: |
+/// | O(N<sup>2</sup>) | 𝛀(N) | 𝚯(N<sup>2</sup>) |
+///
+/// #### Memory Complexity
+/// | Worst | Best | Average |
+/// | :-: | :-: | :-: |
+/// | O(1) | 𝛀(1) | 𝚯(1) |
 ///
 /// # See Also
 /// [Wikipedia](https://en.wikipedia.org/wiki/Odd%E2%80%93even_sort).
