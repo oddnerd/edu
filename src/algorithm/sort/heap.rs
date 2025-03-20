ae9db52739047271005e82f4594ae041e1aa961c
Tyler St. Onge <tyler@stonge.dev>
2025-03-20 13:20:46 -0400

    test: use BDD style naming

--- src/algorithm/sort/heap.rs
+++ src/algorithm/sort/heap.rs
@@ -374,7 +374,7 @@ mod test {
         use super::*;
 
         #[test]
-        fn empty() {
+        fn handles_when_input_is_empty() {
             let mut elements: [usize; 0] = [];
 
             bottom_up(&mut elements);
@@ -383,7 +383,7 @@ mod test {
         }
 
         #[test]
-        fn single_element() {
+        fn handles_when_input_is_single_element() {
             let mut elements = [0];
 
             bottom_up(&mut elements);
@@ -392,8 +392,9 @@ mod test {
         }
 
         #[test]
-        fn already_sorted() {
+        fn does_not_modify_input_when_already_sorted() {
             let mut elements = [0, 1, 2, 3, 4, 5];
+            debug_assert!(elements.is_sorted());
 
             bottom_up(&mut elements);
 
@@ -401,7 +402,7 @@ mod test {
         }
 
         #[test]
-        fn must_swap() {
+        fn will_swap_elements_if_in_decreasing_order() {
             let mut elements = [1, 0];
 
             bottom_up(&mut elements);
@@ -410,7 +411,7 @@ mod test {
         }
 
         #[test]
-        fn odd_length() {
+        fn correctly_orders_elements_when_input_has_odd_length() {
             let mut elements = [2, 1, 0];
 
             bottom_up(&mut elements);
@@ -419,7 +420,7 @@ mod test {
         }
 
         #[test]
-        fn multiple_swaps() {
+        fn correctly_orders_elements_when_input_has_even_length() {
             let mut elements = [2, 0, 3, 1];
 
             bottom_up(&mut elements);
