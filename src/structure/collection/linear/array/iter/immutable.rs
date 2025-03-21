7f2580a60bfd907b52b62cf0de3f93708d4452fe
Tyler St. Onge <tyler@stonge.dev>
2025-03-20 23:20:04 -0400

    doc: best, worst, and average performance

--- src/structure/collection/linear/array/iter/immutable.rs
+++ src/structure/collection/linear/array/iter/immutable.rs
@@ -49,7 +49,15 @@ impl<'a, T: 'a> Iterator for Iter<'a, T> {
     /// Obtain the next element from the front.
     ///
     /// # Performance
-    /// This methods takes O(1) time and consumes O(1) memory.
+    /// #### Time Complexity
+    /// | Worst | Best | Average |
+    /// | :-: | :-: | :-: |
+    /// | O(1) | 𝛀(1) | 𝚯(1) |
+    ///
+    /// #### Memory Complexity
+    /// | Worst | Best | Average |
+    /// | :-: | :-: | :-: |
+    /// | O(1) | 𝛀(1) | 𝚯(1) |
     fn next(&mut self) -> Option<Self::Item> {
         (self.count > 0).then(|| {
             // SAFETY:
