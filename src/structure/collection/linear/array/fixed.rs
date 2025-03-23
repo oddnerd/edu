a01bd4f8d0a00cbe8171fd6569d7486645b9b612
Tyler St. Onge <tyler@stonge.dev>
2025-03-22 22:12:43 -0400

    doc: worst, best, and average performance

--- src/structure/collection/linear/array/fixed.rs
+++ src/structure/collection/linear/array/fixed.rs
@@ -492,7 +492,15 @@ impl<T, const N: usize> DoubleEndedIterator for IntoIter<T, N> {
     /// Obtain the final element, if there are any left.
     ///
     /// # Performance
-    /// This methods takes O(1) time and consumes O(1) memory.
+    /// #### Time Complexity
+    /// | Worst | Best | Average |
+    /// | :-: | :-: | :-: |
+    /// | O(1) | 𝛀(1) | 𝚯(1) |
+    ///
+    /// #### Memory Complexity
+    /// | Worst | Best | Average |
+    /// | :-: | :-: | :-: |
+    /// | O(1) | 𝛀(1) | 𝚯(1) |
     ///
     /// # Examples
     /// ```
