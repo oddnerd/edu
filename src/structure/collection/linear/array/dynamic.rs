0dc3a08c0bfec4c02030604a9c25cffdad7bfdc8
Tyler St. Onge <tyler@stonge.dev>
2025-05-12 20:43:44 -0400

    doc: explain different performance cases

--- src/structure/collection/linear/array/dynamic.rs
+++ src/structure/collection/linear/array/dynamic.rs
@@ -472,15 +472,11 @@ impl<T> Dynamic<T> {
     /// Yields [`FailedAllocation`] when memory (re)allocation fails.
     ///
     /// # Performance
-    /// #### Time Complexity
-    /// | Worst | Best | Average |
-    /// | :-: | :-: | :-: |
-    /// | O(N) | 𝛀(1) | 𝚯(N) |
-    ///
-    /// #### Memory Complexity
-    /// | Worst | Best | Average |
-    /// | :-: | :-: | :-: |
-    /// | O(N) | 𝛀(1) | 𝚯(N) |
+    /// In the best case when [`Self::capacity`] is `capacity` or less, this
+    /// method consumes 𝛀(1) memory and takes 𝛀(1) time. In the worst case
+    /// when reallocation is necessary, this method consumes O(N) memory and
+    /// takes O(N) time. On average, this method consumes 𝚯(N) memory and takes
+    /// 𝚯(N) time.
     ///
     /// # Examples
     /// ```
