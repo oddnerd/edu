82ffc9ccc0c8e12bc0816a78c0dd9dd9cea62cb3
Tyler St. Onge <tyler@stonge.dev>
2025-05-15 22:56:04 -0400

    doc: remove private example

--- src/structure/collection/linear/array/dynamic.rs
+++ src/structure/collection/linear/array/dynamic.rs
@@ -2675,20 +2675,6 @@ impl<T, F: FnMut(&T) -> bool> DoubleEndedIterator for Withdraw<'_, T, F> {
     ///
     /// # Performance
     /// This methods takes O(N) time and consumes O(1) memory.
-    ///
-    /// # Examples
-    /// ```
-    /// use rust::structure::collection::linear::List;
-    /// use rust::structure::collection::linear::array::Dynamic;
-    ///
-    /// let mut underlying = Dynamic::from_iter([0, 1, 2, 3, 4, 5]);
-    /// let mut actual = underlying.withdraw(|element| element % 2 == 0);
-    ///
-    /// assert_eq!(actual.next_back(), Some(4));
-    /// assert_eq!(actual.next_back(), Some(2));
-    /// assert_eq!(actual.next_back(), Some(0));
-    /// assert_eq!(actual.next_back(), None);
-    /// ```
     fn next_back(&mut self) -> Option<Self::Item> {
         while self.remaining > 0 {
             if let Some(decremented) = self.remaining.checked_sub(1) {
