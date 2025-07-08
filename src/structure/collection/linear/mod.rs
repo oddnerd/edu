1d36df7efe127b3350c283afd9aa195a4e53056b
Tyler St. Onge <tyler@stonge.dev>
2025-07-08 03:21:11 -0400

    doc: refine method descriptions

--- src/structure/collection/linear/mod.rs
+++ src/structure/collection/linear/mod.rs
@@ -63,53 +63,87 @@ use super::Collection;
 ///
 /// [zbi]: https://en.wikipedia.org/wiki/Zero-based_numbering
 pub trait Linear: Collection + core::ops::IndexMut<usize, Output = Self::Element> {
-    /// Iterate over the elements by immutable reference.
-    #[must_use]
-    fn iter(
-        &self,
-    ) -> impl DoubleEndedIterator<Item = &Self::Element> + ExactSizeIterator + core::iter::FusedIterator;
-
-    /// Iterate over the elements by mutable reference.
-    #[must_use]
-    fn iter_mut(
-        &mut self,
-    ) -> impl DoubleEndedIterator<Item = &mut Self::Element>
-    + ExactSizeIterator
-    + core::iter::FusedIterator;
-
-    /// Obtain an immutable reference to them `index` element, bounds checked.
+    /// Access an element at a particular `index`.
     #[must_use]
     fn at(&self, index: usize) -> Option<&Self::Element> {
-        (index < self.count()).then(|| &self[index])
+        (index < self.cardinality()).then(|| &self[index])
     }
 
-    /// Obtain a mutable reference to the `index` element, bounds checked.
+    /// Access an element at a particular `index`.
     #[must_use]
     fn at_mut(&mut self, index: usize) -> Option<&mut Self::Element> {
-        (index < self.count()).then(|| &mut self[index])
+        (index < self.cardinality()).then(|| &mut self[index])
     }
 
-    /// Query the element considered to be at the front, the first element.
+    /// Access the element with the lowest index.
     #[must_use]
     fn first(&self) -> Option<&Self::Element> {
         self.at(0)
     }
 
-    /// Query the element considered to be at the back, the last element.
-    #[must_use]
-    fn last(&self) -> Option<&Self::Element> {
-        self.at(self.count().saturating_sub(1))
-    }
-
-    /// Obtain a reference to the element at the front, the first element.
+    /// Access the element with the lowest index.
     #[must_use]
     fn first_mut(&mut self) -> Option<&mut Self::Element> {
         self.at_mut(0)
     }
 
-    /// Obtain a reference to the element at the back, the last element.
+    /// Access the element with the greatest index.
+    #[must_use]
+    fn last(&self) -> Option<&Self::Element> {
+        self.at(self.cardinality().saturating_sub(1))
+    }
+
+    /// Access the element with the greatest index.
     #[must_use]
     fn last_mut(&mut self) -> Option<&mut Self::Element> {
-        self.at_mut(self.count().saturating_sub(1))
+        self.at_mut(self.cardinality().saturating_sub(1))
     }
+
+    // Construct and [`Iterator`] to advance through all contained elements.
+    #[must_use]
+    fn iter(&self) -> impl DoubleEndedIterator<Item = &Self::Element> + ExactSizeIterator + core::iter::FusedIterator;
+
+    // Construct and [`Iterator`] to advance through all contained elements.
+    #[must_use]
+    fn iter_mut(&mut self) -> impl DoubleEndedIterator<Item = &mut Self::Element> + ExactSizeIterator + core::iter::FusedIterator;
+}
+
+// TODO
+trait LinearMut {
+    /// Give [`self`] control of `element`'s lifetime.
+    ///
+    /// This will place `element` at position `index` relative to the already
+    /// contained elements, shifting elements `[index..]` one position right.
+    fn insert(&mut self, index: usize, element: Self::Element) -> Result<&mut Self::Element, Self::Element>;
+
+    // Insert `element` such that it has the smallest index.
+    fn prepend(&mut self, element: Self::Element) -> Result<&mut Self::Element, Self::Element> {
+        self.insert(0, element)
+    }
+
+    // Insert `element` such that it has the largest index.
+    fn append(&mut self, element: Self::Element) -> Result<&mut Self::Element, Self::Element> {
+        self.insert(self.len(), element)
+    }
+
+    /// Take control of the element at `index`'s lifetime away from [`self`].
+    fn remove(&mut self, index: usize) -> Option<Self::Element>;
+
+    // Remove the element with the smallest index.
+    fn remove_first(&mut self) -> Option<Self::Element> {
+        self.next()
+    }
+
+    // Remove the element with the greatest index.
+    fn remove_last(&mut self) -> Option<Self::Element> {
+        self.next_back()
+    }
+
+    // Construct an [`Iterator`] that removes elements within an index `range`.
+    #[must_use]
+    fn drain(&mut self, range: impl core::ops::RangeBounds<usize>) -> impl DoubleEndedIterator<Item = Self::Element> + ExactSizeIterator;
+
+    // Construct an [`Iterator`] that removes elements matching a `predicate`.
+    #[must_use]
+    fn withdraw(&mut self, predicate: impl FnMut(&Self::Element) -> bool) -> impl DoubleEndedIterator<Item = Self::Element>;
 }
