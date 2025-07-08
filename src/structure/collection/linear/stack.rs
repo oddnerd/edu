6947dc81aee202b11c1a6c32e9755ce3ac8cc100
Tyler St. Onge <tyler@stonge.dev>
2025-07-08 02:29:30 -0400

    doc: elaborate on description

--- src/structure/collection/linear/stack.rs
+++ src/structure/collection/linear/stack.rs
@@ -5,6 +5,11 @@ use super::Linear;
 
 /// A [`Linear`] [`Collection`] with last-in-first-out (LIFO) semantics.
 ///
+/// Elements are physically ordered by the relative insertion order and
+/// accessed in that order such that elements can be [`Self::pop`]ped removing
+/// an element at a time from most to least recently inserted by
+/// [`Self::push`]ing.
+///
 /// See also: [Wikipedia](https://en.wikipedia.org/wiki/Stack_(abstract_data_type)).
 pub trait Stack: Collection + Linear {
     /// Add a new element at the top of the stack.
